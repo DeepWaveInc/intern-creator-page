3ccec95d3bd2271a0f76d09d30c6b1ad9a38168b
May Chen <mayflora777@gmail.com>
2023-06-05 13:31:50 +0800

    feat: creatorlist

--- src/pages/Creator/component/CreatorList/CreatorList.jsx
+++ src/pages/Creator/component/CreatorList/CreatorList.jsx
@@ -1,8 +1,46 @@
 import './CreatorList.scss'
+import { Normal as Button } from '../../../../component/button'
+import IMAGES from './media'
+
+
+const ImageList = ({ images }) => {
+    return (
+      <ul className="CreatorList__image">
+        {images.map((image, index) => (
+          <li className="CreatorList__image_item" key={index}>
+            
+            <a href={image.link}>
+                <img 
+                src={image.src} 
+                srcSet={image.srcset} 
+                alt={image.link}
+                />
+            </a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
 const CreatorList = () => {
+
+    
     return(
-        <p>aaa</p>
+        <section className='CreatorList'>
+            <div className='CreatorList__container'>
+                <h1>合作創作者</h1>
+                
+                <ImageList images={IMAGES} />
+
+                <Button
+                    {...{
+                    type: 'primary',
+                    }}>
+                    <span>申請加入創作者社群</span>
+                </Button>
+                
+            </div>
+        </section>
     )}
 
     export default CreatorList
---
+++ src/pages/Creator/component/CreatorList/media.js
@@ -0,0 +1,90 @@
+import CreatorImage_0 from '../../../../assets/image/landing/creator/creator_0.jpg'
+import CreatorImage_1 from '../../../../assets/image/landing/creator/creator_1.jpg'
+import CreatorImage_2 from '../../../../assets/image/landing/creator/creator_2.jpg'
+import CreatorImage_3 from '../../../../assets/image/landing/creator/creator_3.jpg'
+import CreatorImage_4 from '../../../../assets/image/landing/creator/creator_4.jpg'
+import CreatorImage_5 from '../../../../assets/image/landing/creator/creator_5.jpg'
+import CreatorImage_6 from '../../../../assets/image/landing/creator/creator_6.jpg'
+import CreatorImage_7 from '../../../../assets/image/landing/creator/creator_7.jpg'
+import CreatorImage_8 from '../../../../assets/image/landing/creator/creator_8.jpg'
+import CreatorImage_9 from '../../../../assets/image/landing/creator/creator_9.jpg'
+import CreatorImage_10 from '../../../../assets/image/landing/creator/creator_10.jpg'
+import CreatorImage_11 from '../../../../assets/image/landing/creator/creator_11.jpg'
+
+import CreatorImage_0_2x from '../../../../assets/image/landing/creator/creator_0@2x.jpg'
+import CreatorImage_1_2x from '../../../../assets/image/landing/creator/creator_1@2x.jpg'
+import CreatorImage_2_2x from '../../../../assets/image/landing/creator/creator_2@2x.jpg'
+import CreatorImage_3_2x from '../../../../assets/image/landing/creator/creator_3@2x.jpg'
+import CreatorImage_4_2x from '../../../../assets/image/landing/creator/creator_4@2x.jpg'
+import CreatorImage_5_2x from '../../../../assets/image/landing/creator/creator_5@2x.jpg'
+import CreatorImage_6_2x from '../../../../assets/image/landing/creator/creator_6@2x.jpg'
+import CreatorImage_7_2x from '../../../../assets/image/landing/creator/creator_7@2x.jpg'
+import CreatorImage_8_2x from '../../../../assets/image/landing/creator/creator_8@2x.jpg'
+import CreatorImage_9_2x from '../../../../assets/image/landing/creator/creator_9@2x.jpg'
+import CreatorImage_10_2x from '../../../../assets/image/landing/creator/creator_10@2x.jpg'
+import CreatorImage_11_2x from '../../../../assets/image/landing/creator/creator_11@2x.jpg'
+
+const IMAGES = [
+  {
+    src: CreatorImage_0,
+    srcSet: `${CreatorImage_0} 1x,${CreatorImage_0_2x} 2x`,
+    link: 'https://www.facebook.com/travelshutter35/'
+  },
+  {
+    src: CreatorImage_1,
+    srcSet: `${CreatorImage_1} 1x,${CreatorImage_1_2x} 2x`,
+    link: 'https://podcasts.apple.com/tw/podcast/%E6%A5%B5%E9%99%90%E7%99%BD%E6%97%A5%E5%A4%A2-by-beouter/id1575410931'
+  },
+  {
+    src: CreatorImage_2,
+    srcSet: `${CreatorImage_2} 1x,${CreatorImage_2_2x} 2x`,
+    link: 'https://open.firstory.me/user/girlsonwave'
+  },
+  {
+    src: CreatorImage_3,
+    srcSet: `${CreatorImage_3} 1x,${CreatorImage_3_2x} 2x`,
+    link: 'https://www.facebook.com/LightTri/'
+  },
+  {
+    src: CreatorImage_4,
+    srcSet: `${CreatorImage_4} 1x,${CreatorImage_4_2x} 2x`,
+    link: 'https://open.firstory.me/user/beerseatalks'
+  },
+  {
+    src: CreatorImage_5,
+    srcSet: `${CreatorImage_5} 1x,${CreatorImage_5_2x} 2x`,
+    link: 'https://podcasts.apple.com/us/podcast/%E6%8D%95%E6%8D%89%E9%87%8E%E7%94%9F%E7%9A%84%E6%9D%8E%E6%AD%90%E9%82%A3x-%E9%98%BF%E4%BA%AE/id1540891505'
+  },
+  {
+    src: CreatorImage_6,
+    srcSet: `${CreatorImage_6} 1x,${CreatorImage_6_2x} 2x`,
+    link: ''
+  },
+  {
+    src: CreatorImage_7,
+    srcSet: `${CreatorImage_7} 1x,${CreatorImage_7_2x} 2x`,
+    link: 'https://podcasts.apple.com/tw/podcast/%E5%86%B7%E6%B0%A3%E6%88%BF%E5%B0%91%E5%A5%B3-%E6%88%B6%E5%A4%96%E7%B3%BB%E5%B0%91%E5%A5%B3/id1621484039'
+  },
+  {
+    src: CreatorImage_8,
+    srcSet: `${CreatorImage_8} 1x,${CreatorImage_8_2x} 2x`,
+    link: 'https://travelingdrug.com/'
+  },
+  {
+    src: CreatorImage_9,
+    srcSet: `${CreatorImage_9} 1x,${CreatorImage_9_2x} 2x`,
+    link: 'https://www.youtube.com/c/hearingaction'
+  },
+  {
+    src: CreatorImage_10,
+    srcSet: `${CreatorImage_10} 1x,${CreatorImage_10_2x} 2x`,
+    link: 'https://www.facebook.com/unlock.the.earth.travel/'
+  },
+  {
+    src: CreatorImage_11,
+    srcSet: `${CreatorImage_11} 1x,${CreatorImage_11_2x} 2x`,
+    link: 'https://www.youtube.com/@cuonglpnd'
+  }
+]
+
+export default IMAGES
