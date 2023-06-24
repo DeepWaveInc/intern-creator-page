b81db9297ac46cc83b520c971191e1e5b5760f00
May Chen <mayflora777@gmail.com>
2023-06-24 17:24:36 +0800

    style: Application rwd

--- src/pages/Creator/component/Application/Application.jsx
+++ src/pages/Creator/component/Application/Application.jsx
@@ -15,40 +15,79 @@ const Application = () => {
             </div>
             <div className='application__form'>
               <form action="" className='application__form_content'>
-                <div className='block1'>
-                  <div className="part">
+                
+                 
+                <div className="block">
+
+                  <div className='part'>
                     <label>聯絡人</label>
-                    <input type="text"   name=""placeholder="請輸入內容"/>
+                    <input type="text"   name=""placeholder=" 請輸入內容"/>
+                  </div>
+
+                  <div className='part'>
+                    <label>頻道/團隊名稱</label>
+                    <input type="text"   name=""placeholder=" 請輸入內容"/>
+                  </div>
+
+                </div >
+                
+                <div className="block">
+
+                  <di className='part' >
                     <label>頻道連結</label>
-                    <input type="text"  name="" placeholder="請輸入內容"/>
-                    
-                    <label>國籍</label>
-                    <select name=""  ><option selected value disabled>請選擇類型</option></select>
-                  </div >
-                  <div className="part">
-                  <label>頻道/團隊名稱</label>
-                    <input type="text"   name=""placeholder="請輸入內容"/>
+                    <input type="text"  name="" placeholder=" 請輸入內容"/>
+                  </di>
+
+                  <div className='part'>
                     <label>信箱</label>
-                    <input type="text"  name="" placeholder="請輸入內容"/>
-                    
+                    <input type="text"  name="" placeholder=" 請輸入內容"/>                  
+                  </div>
+                  
+                </div>
+
+                <div className="block">
+
+                  <div className='part'>                      
+                    <label>國籍</label>
+                    <select name=""  ><option selected value disabled> 請選擇類型</option></select>
+                  </div>
+                  
+                  <div className='part'>
                     <label>語言</label>
-                    <select name="" ><option selected value disabled>請選擇類型</option></select>
+                    <select name="" ><option selected value disabled> 請選擇類型</option></select>
+                  </div>
+
+                </div>
+              
+                <div className="block">
+                  <div className='part'>
+                    <label>頻道性質</label>
+                    <select name="" ><option selected value disabled> 請選擇類型</option></select>
                   </div>
                 </div>
-                <div className='block2'>
-                  <label>頻道性質</label>
-                  <select name="" ><option selected value disabled>請選擇類型</option></select>
+
+                <div className="block">
+                  <div className='part'>
+
                   <label>加入原因</label>
                   <textarea name=""></textarea>
-
+                  </div>
                 </div>
-                <div className='block3'>
+
+                
+                <div className='block2'>
                   <div className="part">
                       <label>想申請的計畫 </label>
-                      <div className='checkpart'>
+                      <div className='check'>
+                        <div className='checkpart'>
 
-                        <input className="check" type="checkbox" name="" value=""/>  <label className="checklabel" >創作者體驗計畫</label>   
-                        <input className="check" type="checkbox" name="" value=""/> <label className="checklabel">創作者長期購買方案</label>
+                          <input className="checkbox" type="checkbox" name="" value=""/> 
+                          <label className="checklabel" >創作者體驗計畫</label> 
+                        </div>
+                        <div className='checkpart'>
+                          <input className="checkbox" type="checkbox" name="" value=""/> 
+                          <label className="checklabel">創作者長期購買方案</label>
+                        </div>  
                       </div>
                       
                   </div >
@@ -56,18 +95,19 @@ const Application = () => {
                   <div className="part">
                     <div className='buttonpart'>
 
-                  <Button className='applybutton'
+                      <Button className='applybutton'
                             {...{
                             type: 'primary',
                             }}>
                             <span>申請加入</span>
-                        </Button>
+                      </Button>
                     </div>
 
-                        <p className='buttontext'>（申請後將有專人與您聯絡）</p>
-
                   </div>
+
+                  
                 </div>
+                <p className='buttontext'>（申請後將有專人與您聯絡）</p>
               </form>
 
             </div>
